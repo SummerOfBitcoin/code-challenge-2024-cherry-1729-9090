6e55582b9b9fa738821500c3e8ab187137497545
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-04-22 12:04:53 +0530

    trail-6

--- mine.js
+++ mine.js
@@ -8,21 +8,24 @@ function mine(block_header) {
     let nonce = 0;
     const target = Buffer.from("0000ffff00000000000000000000000000000000000000000000000000000000"); // Convert target to buffer
     let paddedNonce = nonce.toString(16).padStart(8, '0'); // Pad nonce with leading zeros to make it 4 bytes
-
     while (true) {
+
         const hash = Buffer.from(littleEndian(doubleHash(block_header + littleEndian(paddedNonce))), 'hex'); // Convert hash to buffer
-        if (hash.compare(target) < 0) { // Compare hashes using buffer compare method
+        // console.log("***")
+        if (target.compare(hash) > 0) { // Compare hashes using buffer compare method
+            console.log(paddedNonce)
             return block_header + littleEndian(paddedNonce);
+
         }
+
         nonce++;
         paddedNonce = nonce.toString(16).padStart(8, '0'); // Update nonce with leading zeros
     }
 }
 
 let mined_block = mine(block_header);
+// console.log(mined_block);
 
-
-console.log(mined_block);
 
 
 
