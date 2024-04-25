6cc54d225c0f71cfb5b516aa8fb136bed4466beb
Devi Charan <145871325+cherry-1729-9090@users.noreply.github.com>
2024-04-25 13:09:28 +0530

    clean trial 2

--- functions.js
+++ functions.js
@@ -5,7 +5,7 @@ const ecdsa = new EC('secp256k1');
 const { createHash } = require('crypto');
 const { verify } = require('crypto');
 const {sha256} = require('./hashes')
-
+const {parseDER,verifyECDSASignature} = require('./EcdsaVerification')
 
 function toHex(value) {
     return value.toString(16).padStart((value.toString(16).length % 2 === 0) ? value.toString(16).length : value.toString(16).length + 1, '0');
@@ -91,32 +91,32 @@ function createDigest(transaction, index) {
 }
 
 
-function parseDER(serialized) {
-    // Extract the length of the R element
-    const rLength = parseInt(serialized.substring(6, 8), 16) * 2;
-    // Calculate the start and end positions of R
-    const rStart = 8;
-    const rEnd = rStart + rLength;
-    // Extract R
-    const r = serialized.substring(rStart, rEnd);
+// function parseDER(serialized) {
+//     // Extract the length of the R element
+//     const rLength = parseInt(serialized.substring(6, 8), 16) * 2;
+//     // Calculate the start and end positions of R
+//     const rStart = 8;
+//     const rEnd = rStart + rLength;
+//     // Extract R
+//     const r = serialized.substring(rStart, rEnd);
   
-    // Extract the length of the S element
-    const sLength = parseInt(serialized.substring(rEnd + 2, rEnd + 4), 16) * 2;
-    // Calculate the start and end positions of S
-    const sStart = rEnd + 4;
-    const sEnd = sStart + sLength;
-    // Extract S
-    const s = serialized.substring(sStart, sEnd);
-    return { r, s };
-  }
+//     // Extract the length of the S element
+//     const sLength = parseInt(serialized.substring(rEnd + 2, rEnd + 4), 16) * 2;
+//     // Calculate the start and end positions of S
+//     const sStart = rEnd + 4;
+//     const sEnd = sStart + sLength;
+//     // Extract S
+//     const s = serialized.substring(sStart, sEnd);
+//     return { r, s };
+//   }
 
-  function verifyECDSASignature(publicKeyHex, signatureHex, messageHex) {
-    const ecdsa = new EC("secp256k1");
-    const key = ecdsa.keyFromPublic(publicKeyHex, "hex");
-    const signature = parseDER(signatureHex);
-    const isValid = key.verify(messageHex, signature);
-    return isValid;
-  }
+//   function verifyECDSASignature(publicKeyHex, signatureHex, messageHex) {
+//     const ecdsa = new EC("secp256k1");
+//     const key = ecdsa.keyFromPublic(publicKeyHex, "hex");
+//     const signature = parseDER(signatureHex);
+//     const isValid = key.verify(messageHex, signature);
+//     return isValid;
+//   }
   
   function checkSigP2PKH(transaction, i) {
     const message = createDigest(transaction, i);
